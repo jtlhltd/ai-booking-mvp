f9943a993f51761500b48cdd979d2d7f5ff643fc
Jonah Hughes <jonahthomaslloydhughes@gmail.com>
2026-05-06 12:05:09 +0100

    fix(server): import express for webhook parsers
    Fix production crash: express was referenced for urlencoded webhook bodies but not imported.

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import { nanoid } from 'nanoid';
 import { DateTime } from 'luxon';
 import * as chrono from 'chrono-node';
 import { createServer } from 'http';
+import express from 'express';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 import compression from 'compression';
